9f8457a3b75a5976c4ea8a3d9bbba579229cb578
Claudio Vallejo <claudio.vallejo@gmail.com>
2018-08-16 15:30:13 -0300

    arreglo nombres

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Vallejo.wine</title>
+        <title>vallejo.wine</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -70,7 +70,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Vallejo.wine
+                    vallejo.wine
                 </div>
 
                 <div class="links">
